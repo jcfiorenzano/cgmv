e6609e7d59d0fad6ad74ec90b29c22b05084d93f
Juan Fiorenzano <jufioren@microsoft.com>
2020-05-22 09:36:21 -0700

    init

---
+++ cgmv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cgmv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
